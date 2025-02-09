216e702826ad4e55d06dcb26067b5d427d141a00
HirumiN <hilminurullah3@gmail.com>
2025-02-10 05:19:15 +0700

    category

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,16 @@
 
 use App\Http\Controllers\BoardingHouseController;
 use App\Http\Controllers\BookingController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/check-booking', [BookingController::class, 'check'])->name('check-booking');
+
+Route::get('/category/{slug}', [CategoryController::class, 'show'])->name('category-show');
+
 
 Route::get('/find-kos', [BoardingHouseController::class, 'find'])->name('find-kos');
 Route::get('/find-result', [BoardingHouseController::class, 'findResult'])->name('find-kos.result');
